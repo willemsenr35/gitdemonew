b86e0392051323dd381c9f22dceb156a196bc9f5
willemsenr35 <31731256+willemsenr35@users.noreply.github.com>
2017-09-08 13:38:19 +0530

    Update print.java

--- gitdemo/src/gitdemo/print.java
+++ gitdemo/src/gitdemo/print.java
@@ -1,11 +1,6 @@
-package gitdemo;
-
-public class print {
-
+public class HelloWorld
+{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("test");
-
+		System.out.println("Hello World!");
 	}
-
 }
