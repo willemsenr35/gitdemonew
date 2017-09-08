20c1f0d7e76861a714c7af8316a1ef3506411251
willemsenr35 <31731256+willemsenr35@users.noreply.github.com>
2017-09-08 14:59:42 +0530

    Update Factorial.java

--- gitdemo/src/gitdemo/Factorial.java
+++ gitdemo/src/gitdemo/Factorial.java
@@ -1,7 +1,8 @@
 public class Factorial
 {
 	public static void main(String[] args)
-	{	final int NUM_FACTS = 100;
+	{	System.out.println("sanjeev");
+		final int NUM_FACTS = 100;
 		for(int i = 0; i < NUM_FACTS; i++)
 			System.out.println( i + "! is " + factorial(i));
 	}
